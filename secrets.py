75871a7572066efd0c92ac5b192353297ef1ad67
bgold98 <golding@umich.edu>
2018-02-06 09:54:05 -0500

    second

--- secrets.py
+++ secrets.py
@@ -1,2 +1,2 @@
-api_key = sTpk7UEjMYOFx613JEDFQqgAa
-api_secret = Qh6pJMwh3qIsKVZiGlsGMWVqr5ngiP5s7jjbXtmCL6o0DWSrF2
+client_key = "sTpk7UEjMYOFx613JEDFQqgAa"
+client_secret = "Qh6pJMwh3qIsKVZiGlsGMWVqr5ngiP5s7jjbXtmCL6o0DWSrF2"
